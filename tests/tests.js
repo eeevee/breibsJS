934e332b9ff73a530e0bf9b12e9429f5fb39d04c
eeevee <eeevee@gmail.com>
2014-03-19 01:49:02 -0300

    - Test file, with a few Core Module Tests

---
+++ tests/tests.js
@@ -0,0 +1,45 @@
+var core;
+var scene1;
+var SCENE_1_NAME = 'scene1';
+
+module("Core", {
+	setup: function() {
+		core = new Core();
+		scene1 = new Scene('scene1');
+		core.addScene(scene1);
+	}, teardown: function() {
+
+	}
+});
+
+test("When I start the Core, I need to have a root scene", function() {
+	ok(core.rootScene);
+	ok(core.getScene('root'));
+});
+
+test("When I add a scene to Core, i need to recover it by name", function() {
+	ok(core.getScene(SCENE_1_NAME));
+});
+
+test("When I change the scene, the Core current scene want to be the new scene", function() {
+	core.changeScene(SCENE_1_NAME);
+	ok(core.currentScene === scene1);
+});
+
+test("When I try to change to a scene that not exists, the Core need to throw a  Scene not found CoreException", function() {
+	throws(
+		function() {
+			core.changeScene('not real scene')
+		},
+			CoreException,
+			'raised error is CoreException'
+	);
+
+	throws(
+		function() {
+			core.changeScene('not real scene')
+		},
+			/Scene not found/,
+			'raised error message is Scene not found'
+	);
+});
