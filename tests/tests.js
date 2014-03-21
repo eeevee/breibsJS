eb785a7c3564508e22603558384fb45e5934bede
eeevee <eeevee@gmail.com>
2014-03-21 02:20:53 -0300

    - Add test to preload image

--- tests/tests.js
+++ tests/tests.js
@@ -5,6 +5,7 @@ var SCENE_1_NAME = 'scene1';
 module("Core", {
 	setup: function() {
 		core = new Core();
+		core.init();
 		scene1 = new Scene('scene1');
 		core.addScene(scene1);
 	}, teardown: function() {
@@ -42,4 +43,14 @@ test("When I try to change to a scene that not exists, the Core need to throw a 
 			/Scene not found/,
 			'raised error message is Scene not found'
 	);
+});
+
+asyncTest("When I preload a image, when the image load finished, i need to retrive the image", function() {
+	expect(1);
+
+	core.addEventListener('load', function(e) {
+		ok(core.assets['../img/wizard_evil.png']);
+		start();
+	});
+	core.preloadAsset('../img/wizard_evil.png');
 });
