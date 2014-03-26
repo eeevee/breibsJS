9837871bb69dd3162cbc5443c8f7b401284a84ac
Pedro <eeevee@gmail.com>
2014-03-26 13:02:25 -0300

    Added Ease equations and suport to it in Tween class

--- js/Tween.js
+++ js/Tween.js
@@ -12,10 +12,13 @@ var Tween = function()
 	this.interval;
 	this.steps;
 
-	this.moveTo = function(target, x, y, frames) {
+	this.moveTo = function(target, x, y, frames, ease) {
+		if (ease == null) {
+			ease = Ease.linear;
+		}
 		this.target = target;
-		this.properties['x'] = {startPos: target.x, endPos: x};
-		this.properties['y'] = {startPos: target.y, endPos: y};
+		this.properties['x'] = {startPos: target.x, endPos: x, ease: ease};
+		this.properties['y'] = {startPos: target.y, endPos: y, ease: ease};
 		this.steps = frames;
 	};
 
@@ -60,14 +63,80 @@ var Tween = function()
 	this.calculateValues = function(properties) {
 		var values = [];
 		for (var i = 0; i < this.steps + 1; i++) {
-			var startValue = properties.startPos;
-			var currentTime = i + 1;
-			var duration = this.steps;
-			var changeInValue = ((properties.endPos - properties.startPos) );
-			//values.push((changeInValue * (currentTime / duration)) + startValue);//linear
-			//values.push((changeInValue * (currentTime /= duration) * currentTime) + startValue);//easeInQuad
-			values.push(-changeInValue * (currentTime /= duration) * (currentTime - 2) + startValue);//easeOutQuad
+			var startValue = properties.startPos;//b
+			var currentTime = i + 1;//t
+			var duration = this.steps;//d
+			var changeInValue = ((properties.endPos - properties.startPos) );//c
+			values.push(properties.ease(currentTime, startValue, changeInValue, duration));
 		}
 		return values;
 	};
+};
+
+var Ease = {
+	linear: function(t, b, c, d) {
+		return c * (t / d) + b;
+	},
+
+	easeInQuad: function(t, b, c, d) {
+		return c * ( t/= d) * t + b;
+	},
+
+	easeOutQuad: function(t, b, c, d) {
+		return -c * (t /= d) * (t - 2) + b;
+	},
+
+	easeInOutQuad: function(t, b, c, d) {
+		if ((t /= d / 2) < 1) return c / 2 * t * t + b;
+		return -c / 2 * ((--t) * (t - 2) - 1) + b;
+	},
+
+	easeInElastic: function(t, b, c, d) {
+		var s = 1.70158;
+		var p = 0;
+		var a = c;
+		if (t == 0) return b;  
+		if ((t /= d) == 1) return b + c;
+		if (!p) p = d * .3;
+		if (a < Math.abs(c)) { 
+			a = c; 
+			var s = p / 4;
+		} else {
+			var s = p / (2 * Math.PI) * Math.asin (c / a);
+		}
+		return -(a * Math.pow(2,10 * (t-= 1)) * Math.sin( (t * d - s) * (2 * Math.PI) / p )) + b;
+	},
+
+	easeOutElastic: function(t, b, c, d) {
+		var s = 1.70158;
+		var p = 0;
+		var a = c;
+		if (t == 0) return b;
+		if ((t /= d) == 1) return b + c;
+		if (!p) p = d * .3;
+		if (a < Math.abs(c)) { 
+			a = c; 
+			var s = p / 4; 
+		} else {
+			var s = p / (2 * Math.PI) * Math.asin (c / a);
+		}
+		return a * Math.pow(2, -10 * t) * Math.sin( (t * d - s) * (2 * Math.PI) / p ) + c + b;
+	},
+
+	easeInOutElastic: function(t, b, c, d) {
+		var s = 1.70158;
+		var p = 0;
+		var a = c;
+		if (t == 0) return b;
+		if ((t /= d / 2) == 2) return b + c;
+		if (!p) p = d * (.3 * 1.5);
+		if (a < Math.abs(c)) { 
+			a = c;
+			var s = p / 4; 
+		} else {
+			var s = p / (2 * Math.PI) * Math.asin (c / a);
+		}
+		if (t < 1) return -.5 * (a * Math.pow(2,10 * (t -= 1)) * Math.sin( (t * d - s) * (2 * Math.PI) / p )) + b;
+		return a * Math.pow(2, -10 * (t -= 1)) * Math.sin( (t * d - s) * (2 * Math.PI)/p ) * .5 + c + b;
+	}
 };
