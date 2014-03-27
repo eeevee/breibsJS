33f9830f4fa4ab0629cea1c19a2d0409ffc93634
Pedro <eeevee@gmail.com>
2014-03-27 12:09:23 -0300

    Added inheritance to TweenGroup from EventDispatcher and send a complete event when all the tweens are finished

--- js/TweenGroup.js
+++ js/TweenGroup.js
@@ -1,14 +1,20 @@
 var TweenGroup = function()
 {
+	EventDispatcher.call(this);
+
 	this.tweens = [];
 
 	this.addTween = function(tween) {
 		tween.addEventListener('complete', this.removeTween.bind(this));
 		this.tweens.push(tween);
+		return this;
 	};
 
 	this.removeTween = function(e) {
 		var tweenIndex = this.tweens.indexOf(tween);
 		this.tweens.splice(tweenIndex, 1);
+		if (this.tweens.length == 0) {
+			this.dispatchEvent('complete');
+		}
 	};
 }
