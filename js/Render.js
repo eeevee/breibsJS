016c93ea5ddb932e3c7e63072ff94f89a4955e81
Pedro <eeevee@gmail.com>
2014-03-28 12:22:58 -0300

    Added collision tests

--- js/Render.js
+++ js/Render.js
@@ -1,25 +1,28 @@
 var Render = function(context)
 {
 	this.context = context;
+	this.canRender = true;
 
 	this.clear = function() {
 		this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height);
 	}
 
 	this.drawScene = function(scene) {
-		this.clear();
-		if (!scene) return;
-		for (var i = 0; i < scene.childs.length; i++) {
-			var displayObject = scene.childs[i];
-			if (displayObject.visible) {
-				this.context.save();
-				this.context.globalAlpha = displayObject.opacity;
-				this.context.scale(displayObject.scaleX, displayObject.scaleY);
-				var rotationInRadians = displayObject.rotation * Math.PI / 180;
-				context.rotate(rotationInRadians);
-				this.context.drawImage(displayObject.image, displayObject.x / displayObject.scaleX, displayObject.y / displayObject.scaleY);
-				this.context.globalAlpha = 1;
-				this.context.restore();
+		if (this.canRender) {
+			this.clear();
+			if (!scene) return;
+			for (var i = 0; i < scene.childs.length; i++) {
+				var displayObject = scene.childs[i];
+				if (displayObject.visible) {
+					this.context.save();
+					this.context.globalAlpha = displayObject.opacity;
+					this.context.scale(displayObject.scaleX, displayObject.scaleY);
+					var rotationInRadians = displayObject.rotation * Math.PI / 180;
+					context.rotate(rotationInRadians);
+					this.context.drawImage(displayObject.image, displayObject.x / displayObject.scaleX, displayObject.y / displayObject.scaleY);
+					this.context.globalAlpha = 1;
+					this.context.restore();
+				}
 			}
 		}
 	}
