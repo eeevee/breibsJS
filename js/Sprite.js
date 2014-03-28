09514a3ef8802e4c6130b7f186f36a3abb8072b7
Pedro <eeevee@gmail.com>
2014-03-28 12:55:18 -0300

    Added method getWidth and getHeight to Sprite

--- js/Sprite.js
+++ js/Sprite.js
@@ -5,25 +5,32 @@ var Sprite = function(width, height)
 	this.width = width;
 	this.height = height;
 	this.tweenGroup = new TweenGroup(this);
-
 	this.image;
 
 	this.boxCollides = function(target) {
 		if (!target.visible) return false;
 		if (this.x < target.x + target.width  &&
-			this.x + this.width * this.scaleX > target.x    &&
+			this.x + this.getWidth() > target.x    &&
 			this.y < target.y + target.height &&
-			this.y + this.height * this.scaleY > target.y) {
+			this.y + this.getHeight() > target.y) {
 				return true;
 		}
 		return false;
 	};
 
 	this.pointCollides = function(point) {
-		if (this.x < point.x && this.x + (this.width * this.scaleX) > point.x &&
-			this.y < point.y && this.y + (this.height * this.scaleY) > point.y) {
+		if (this.x < point.x && this.x + this.getWidth() > point.x &&
+			this.y < point.y && this.y + this.getHeight() > point.y) {
 			return true;
 		}
 		return false;
+	};
+
+	this.getWidth = function() {
+		return this.width * this.scaleX;
+	};
+
+	this.getHeight = function() {
+		return this.height * this.scaleY;
 	};
 };
