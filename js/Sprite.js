c48a1d44c5a80a7f6f438785f08b9b8887b62e5d
eeevee <eeevee@gmail.com>
2014-03-28 01:07:55 -0300

    Added colision with point and colision with box methos to Sprite class

--- js/Sprite.js
+++ js/Sprite.js
@@ -7,4 +7,22 @@ var Sprite = function(width, height)
 	this.tweenGroup = new TweenGroup(this);
 
 	this.image;
+
+	this.boxCollides = function(target) {
+		if (this.x < target.x + target.width  &&
+			this.x + this.width * this.scaleX > target.x    &&
+			this.y < target.y + target.height &&
+			this.y + this.height * this.scaleY > target.y) {
+				return true;
+		}
+		return false;
+	};
+
+	this.pointCollides = function(point) {
+		if (this.x < point.x && this.x + (this.width * this.scaleX) > point.x &&
+			this.y < point.y && this.y + (this.height * this.scaleY) > point.y) {
+			return true;
+		}
+		return false;
+	};
 };
