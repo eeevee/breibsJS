3d8fef0b4bd82dfc43b772f5d659bd69aab7eb7f
Pedro <eeevee@gmail.com>
2014-03-28 16:26:56 -0300

    Add flag to say if the canvas must be added to the document

--- js/Surface.js
+++ js/Surface.js
@@ -1,10 +1,14 @@
-var Surface = function(width, height) 
+var Surface = function(width, height, addToDocument) 
 {
+	if (!addToDocument) {
+		addToDocument = true;
+	}
+
 	this.canvas = document.createElement('canvas');
 	this.canvas.width = width;
 	this.canvas.height = height;
 	this.canvas.style.border = "1px solid";
-	document.body.appendChild(this.canvas);
+	if (addToDocument) document.body.appendChild(this.canvas);
 
 	this.context = this.canvas.getContext('2d');
 };
