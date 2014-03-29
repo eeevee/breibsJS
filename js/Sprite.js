ce62bc292fe68a0041270be48d1992bb9c5614cd
eeevee <eeevee@gmail.com>
2014-03-29 17:51:42 -0300

    - Remove unused method from Sprite class

--- js/Sprite.js
+++ js/Sprite.js
@@ -20,22 +20,6 @@ var Sprite = function(width, height)
 		return _collisionManager.pixelPerfectCollides(target);
 	};
 
-	this.generatePixelMap = function(source, resolution) {
-		if (!resolution) resolution = 5;
-		if (!source) source = this;
-		
-		var map = [];
-		for (var y = 0; y < source.height; y += resolution) {
-			for (var x = 0; x < source.width; x += resolution) {
-				var rowOffset = y + "_" + x;
-				var pixel = surface.context.getImageData(x, y, resolution, resolution);
-				var pixelData = pixel.data;
-				map[rowOffset] = {x: x, y: y, pixelData: pixelData};
-			}
-		}
-		return map;
-	};
-
 	//GETTERS
 	this.getScaleX = function() {
 		return this.width / _originalWidth;
