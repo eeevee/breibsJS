602265112fc2174484f8ddb3bf9399a897a27558
eeevee <eeevee@gmail.com>
2014-03-29 19:10:18 -0300

    - Moving scaleX/Y and original width/height to Display Object

--- js/DisplayObject.js
+++ js/DisplayObject.js
@@ -2,6 +2,9 @@ var DisplayObject = function(width, height)
 {
 	EventDispatcher.call(this);
 
+	this._originalWidth = width;
+	this._originalHeight = height;
+
 	this.x = 0;
 	this.y = 0;
 	this.rotation = 0;
@@ -9,4 +12,22 @@ var DisplayObject = function(width, height)
 	this.visible = true;
 	this.width = width;
 	this.height = height;
+
+	//GETTERS
+	this.getScaleX = function() {
+		return this.width / this._originalWidth;
+	};
+
+	this.getScaleY = function() {
+		return this.height / this._originalHeight;
+	};
+
+	//SETTERS
+	this.setScaleX = function(value) {
+		this.width = this._originalWidth * value;
+	};
+
+	this.setScaleY = function(value) {
+		this.height = this._originalHeight * value;
+	};
 }
--- js/Sprite.js
+++ js/Sprite.js
@@ -2,8 +2,6 @@ var Sprite = function(width, height)
 {
 	DisplayObject.call(this, width, height);
 
-	var _originalWidth = width;
-	var _originalHeight = height;
 	var _collisionManager = new CollisionManager(this);
 	this.tweenGroup = new TweenGroup(this);
 	this.image;
@@ -18,23 +16,5 @@ var Sprite = function(width, height)
 
 	this.pixelPerfectCollides = function(target) {
 		return _collisionManager.pixelPerfectCollides(target);
-	};
-
-	//GETTERS
-	this.getScaleX = function() {
-		return this.width / _originalWidth;
-	};
-
-	this.getScaleY = function() {
-		return this.height / _originalHeight;
-	};
-
-	//SETTERS
-	this.setScaleX = function(value) {
-		this.width = _originalWidth * value;
-	};
-
-	this.setScaleY = function(value) {
-		this.height = _originalHeight * value;
 	};
 };
