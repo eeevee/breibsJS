60f1cf3ef091f9cce679a41de459372fe5406a27
eeevee <eeevee@gmail.com>
2014-03-29 17:52:20 -0300

    - Added pixel perfect collision detection

--- js/CollisionManager.js
+++ js/CollisionManager.js
@@ -1,18 +1,26 @@
 var CollisionManager = function(source)
 {
 	var _source = source;
-	var _collisionSurface = new Surface((surface) ? surface.canvas.width : 100, (surface) ? surface.canvas.height : 100, true);
-	var _render = new Render(_collisionSurface.context);
-	var _collisionScene = new Scene('collision');
-	_collisionScene.addChild(source);
+	var _collisionSurfaceSource = new Surface((surface) ? surface.canvas.width : 100, (surface) ? surface.canvas.height : 100, DEBUG);
+	var _collisionSurfaceTarget = new Surface((surface) ? surface.canvas.width : 100, (surface) ? surface.canvas.height : 100, DEBUG);
+	var _renderSource = new Render(_collisionSurfaceSource.context);
+	var _renderTarget = new Render(_collisionSurfaceTarget.context);
+	var _collisionSceneSource = new Scene('collisionSource');
+	var _collisionSceneTarget = new Scene('collisionTarget');
+	_collisionSceneSource.addChild(source);
 
-	this.boxCollides = function(target) {
+	this.boxCollides = function(target, pixel) {
 		if (!target.visible) return false;
 		if (_source.x < target.x + target.width  &&
 			_source.x + _source.width > target.x    &&
 			_source.y < target.y + target.height &&
 			_source.y + _source.height > target.y) {
-				return true;
+				if (!pixel) {
+					return true;
+				} else {
+					return this.pixelPerfectCollides(target);
+				}
+				
 		}
 		return false;
 	};
@@ -26,42 +34,47 @@ var CollisionManager = function(source)
 	};
 
 	this.pixelPerfectCollides = function(target) {
-		_collisionScene.addChild(target);
-		_render.drawScene(_collisionScene);
-		//var myImageData = this.generatePixelMap(this);//surface.context.getImageData(this.x, this.y, this.width, this.height);
-		//var myImageData = surface.context.getImageData(this.x, this.y, this.width, this.height);
-		//var targetImageData = surface.context.getImageData(targetImageData.x, targetImageData.y, targetImageData.width, targetImageData.height);
-		//var targetImageData = this.generatePixelMap(target);//surface.context.getImageData(targetImageData.x, targetImageData.y, targetImageData.width, targetImageData.height);
+		_collisionSceneTarget.addChild(target);
+		_renderSource.drawScene(_collisionSceneSource);
+		_renderTarget.drawScene(_collisionSceneTarget);
 
-		var left = Math.max(_source.x, target.x);
-		var top = Math.max(_source.y, target.y);
-		var right = Math.max(_source.x + _source.width, target.x + target.width);
-		var bottom = Math.max(_source.y + _source.height, target.y + target.height);
+		var intersection = calculateIntersection(_source, target);
+		if (!intersection) return;
+		var sourceImageData = _collisionSurfaceSource.context.getImageData(intersection.x, intersection.y, intersection.width, intersection.height);
+		var targetImageData = _collisionSurfaceTarget.context.getImageData(intersection.x, intersection.y, intersection.width, intersection.height);
 
-		//to ensure that the top-left corner is both above and to the left of the bottom-right corner
-		//if (xMin >= xMax || yMin >= yMax) return false;
-
-		//var xDiff = xMax - xMin;
-		//var yDiff = yMax - yMin;
-		//var myPixels = myImageData.data;
-		//var targetPixels = targetImageData.data;
-
-/*
-		for (var y = top; y < bottom; y++) {
-			for (var x = left; x < right; x++) {
-				var pixel1 = myImageData[(x - this.x) + "_" + (y - this.y)];
-				var pixel2 = targetImageData[(x - target.x) + "_" + (y - target.y)];
-
-				if (!pixel1 || ! pixel2) {
-					continue;
-				}
-
-				if (pixel1.pixelData[3] != 0 && pixel2.pixelData[3] != 0) {
-					return true;
-				}
+		for (var i = 0; i < sourceImageData.data.length; i += 4) {
+			var pixel1 = sourceImageData.data[i + 3];
+			var pixel2 = targetImageData.data[i + 3];
+			if (pixel1 != 0 && pixel2 != 0) {
+				return true;
 			}
 		}
-*/
 		return false;
 	};
+
+	var calculateIntersection = function(rect1, rect2) {
+	    var dx;
+	    var dy;
+	    var r1 = {};
+	    var r2 = {};
+	    r1.centerX = rect1.x + (r1.halfWidth = (rect1.width / 2));
+	    r1.centerY = rect1.y + (r1.halfHeight = (rect1.height/ 2));
+	    r2.centerX = rect2.x + (r2.halfWidth = (rect2.width / 2));
+	    r2.centerY = rect2.y + (r2.halfHeight = (rect2.height/ 2));
+
+	    dx = Math.abs(r1.centerX-r2.centerX) - (r1.halfWidth + r2.halfWidth);
+	    dy = Math.abs(r1.centerY-r2.centerY) - (r1.halfHeight + r2.halfHeight);
+
+	    if (dx < 0 && dy < 0) {
+	      dx = Math.min(Math.min(rect1.width, rect2.width), -dx);
+	      dy = Math.min(Math.min(rect1.height, rect2.height), -dy);
+	      return {x:Math.max(rect1.x, rect2.x),
+	              y:Math.max(rect1.y, rect2.y),
+	              width: dx,
+	              height: dy};
+	    } else {
+	      return null;
+	    }
+  	}
 };
