60102baf1cddb7b2ed9d6e4226c784c8a30947e1
Pedro <eeevee@gmail.com>
2014-04-02 16:05:36 -0300

    Changes to space demo

--- demos/space/js/SpaceDemo.js
+++ demos/space/js/SpaceDemo.js
@@ -33,7 +33,7 @@ var TitleScene = function(name)
 			Keyboard.pressedKeys[Keyboard.KEY_CODES['Space']] = false;
 			core.removeEventListener('keydown', this.keydownHandlerBind);
 			var evt = new Event('changescene');
-			evt.data = this;
+			evt.data = 'game';
 			core.dispatchEvent(evt);
 		}
 	};
@@ -48,30 +48,40 @@ var GameScene = function(name)
 	Scene.call(this, name);
 
 	var hero = new Wizard(16, 16,  core.assets['../img/wizard_evil_no_bg.png']);
-	hero.x = core.gameWidth / 2 - hero.width / 2;
-	hero.y = core.gameHeight - hero.height;
-	this.addChild(hero);
-
 	var enemies = [];
-	for (var x = 0; x < 8; x++) {
-		for (var y = 0; y < 3; y++) {
-			var enemy = new Sprite(16,16);
-			enemy.image = core.assets['../img/wizard_evil_no_bg.png'];
-			enemy.x = 18 * x + 80;
-			enemy.y = 18 * y + 10
-			this.addChild(enemy);
-			enemy.tweenGroup.moveTo(enemy.x + 50, enemy.y, 30).moveTo(enemy.x - 50, enemy.y, 30).setLoop(true);
-			enemies.push(enemy);
-		}
-	}
-
 	var scoreLabel = new TextField('score: 0', 'Arial', 12, 'black', 'left');
-	scoreLabel.x = 10;
-	scoreLabel.y = 20;
-	var score = 0;
-	this.addChild(scoreLabel);
-
+	var score;
 	var bullets = [];
+
+	this.init = function() {
+		while (this.childs.length) {
+			this.removeChildAt(0);
+		}
+
+		score = 23;
+		scoreLabel.text = 'score: ' + score;
+		scoreLabel.x = 10;
+		scoreLabel.y = 20;
+		this.addChild(scoreLabel);
+		enemies = [];
+		bullets = [];
+
+		hero.x = core.gameWidth / 2 - hero.width / 2;
+		hero.y = core.gameHeight - hero.height;
+		this.addChild(hero);
+
+		for (var x = 0; x < 8; x++) {
+			for (var y = 0; y < 3; y++) {
+				var enemy = new Sprite(16,16);
+				enemy.image = core.assets['../img/wizard_evil_no_bg.png'];
+				enemy.x = 18 * x + 80;
+				enemy.y = 18 * y + 10
+				this.addChild(enemy);
+				enemy.tweenGroup.moveTo(enemy.x + 50, enemy.y, 30).moveTo(enemy.x - 50, enemy.y, 30).setLoop(true);
+				enemies.push(enemy);
+			}
+		}
+	};
 
 	this.frameHandler = function _frameHandler(e) {
 		if(Keyboard.pressedKeys[Keyboard.KEY_CODES['Right']]) {
@@ -85,7 +95,7 @@ var GameScene = function(name)
 				hero.x -= 5;
 			}
 		}
-		
+
 		if(Keyboard.pressedKeys[Keyboard.KEY_CODES['Space']]) {
 			if (bullets.length < 1) {
 				var knife = new Knife(16, 16,  core.assets['../img/knife.png']);
@@ -110,23 +120,20 @@ var GameScene = function(name)
 				var wiz2 = enemies[j];
 				//bullet collide with enemy
 				if (bullet.boxCollides(wiz2, false)) {
-  				console.log('colidiu');
   				enemies.splice(enemies.indexOf(wiz2), 1);
   				this.removeChildAt(this.childs.indexOf(wiz2));
   				bullets.splice(bullets.indexOf(bullet), 1);
   				this.removeChildAt(this.childs.indexOf(bullet));
   				score += 1;
   				scoreLabel.text = 'score: ' + score;
-  				// all enemies destroyed, end game screen				
+  				// all enemies destroyed, end game screen		
+  				console.log(score);		
   				if (score == 24) {
-  					var scene = new Scene('End game');
-  					scene.addChild(scoreLabel);
-  					scoreLabel.fontSize = 30;
-  					scoreLabel.x = 80;
-  					scoreLabel.y = 150;
   					this.removeEventListener('enterframe', _frameHandler);
-  					game.addScene(scene);
-  					game.changeScene('End game');
+  					var evt = new Event('changescene');
+					evt.data = 'score';
+					evt.score = score;
+					core.dispatchEvent(evt);
   				}
   				break;
 				}
@@ -134,5 +141,34 @@ var GameScene = function(name)
 		}
 	};
 
+	this.init();
 	this.addEventListener('enterframe', this.frameHandler.bind(this));
+};
+
+var ScoreScene = function(name, score)
+{
+	Scene.call(this, name);
+
+	var scoreLabel = new TextField('score: ' + score, 'Arial', 30, 'black', 'center');
+	scoreLabel.x = core.gameWidth / 2;
+	scoreLabel.y = core.gameHeight / 2;
+  	this.addChild(scoreLabel);
+
+  	this.setLabel = function(label) {
+  		scoreLabel.text = label;
+  	};
+  			
+  	this.keydownHandler = function(e) {
+		if (e.keyCode == Keyboard.KEY_CODES['Space']) {
+			Keyboard.pressedKeys[Keyboard.KEY_CODES['Space']] = false;
+			core.removeEventListener('keydown', this.keydownHandlerBind);
+			var evt = new Event('changescene');
+			evt.data = 'game';
+			core.dispatchEvent(evt);
+		}
+	};
+
+	this.keydownHandlerBind = this.keydownHandler.bind(this);
+
+	core.addEventListener('keydown', this.keydownHandlerBind);
 };
