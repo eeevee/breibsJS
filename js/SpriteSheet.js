9ada9c70bc2880b50740a0af0c722353ab3f37dd
eeevee <eeevee@gmail.com>
2014-04-04 01:23:13 -0300

    - Added animations in spritesheet

--- js/SpriteSheet.js
+++ js/SpriteSheet.js
@@ -3,25 +3,42 @@ var SpriteSheet = function(width, height, image, speed)
 	Sprite.call(this, width, height, image);
 
 	var currentFrame = 0;
+	var animationCount = 0;
 	var count = 0;
 	var framesPerRow = Math.floor(image.width / this.width);
 	var speed = speed;
 
-	this.animations = {}; //32x 41
+	this.animations = {};
 	this.currentAnimation = '';
 
 	this.update = function() {
 		count ++;
 		if (count < speed) return;
 		if (count == speed) count = 0;
-		currentFrame ++;
-		if (false) {
-
+		
+		if (this.currentAnimation.length) {
+			var animation = this.animations[this.currentAnimation];
+			animationCount ++;
+			if (animationCount >= animation.frames.length) {
+				if (animation.hasOwnProperty('next')) {
+					this.setCurrentAnimation(animation['next']);
+				} else {
+					animationCount = 0;
+				}	
+			}
+			currentFrame = animation.frames[animationCount];
 		} else {
+			currentFrame ++;
 			if (currentFrame >= (this.image.width / this.width) * (this.image.height / this.height)) {
 				currentFrame = 0;
 			}
 		}
+	};
+
+	this.setCurrentAnimation = function(animation) {
+		this.currentAnimation = animation;
+		currentFrame = this.animations[animation].frames[0];
+		animationCount = 0;
 	};
 
 	this.getCol = function() {
