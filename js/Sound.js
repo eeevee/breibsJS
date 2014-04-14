a850abd214ba244b04ed51101019a9fa8cacc0ea
Pedro <eeevee@gmail.com>
2014-04-14 13:22:40 -0300

    Added support to loop sound

--- js/Sound.js
+++ js/Sound.js
@@ -1,20 +1,34 @@
 var Sound = function(audio) 
 {
 	this.audio = audio;
-	this.currentTime = 0;
+	this.currentTime = 360;
 
-	this.play = function() {
+	this.play = function(loop) {
+		if (loop == null) {
+			loop = false;
+		}
 		this.audio.currentTime = this.currentTime;
 		this.audio.play();
+		if (loop) {
+			this.audio.addEventListener('ended', this.endSoundHandlerBind);
+		}
 	};
 
 	this.stop = function() {
 		this.currentTime = 0;
 		this.audio.stop();
+		this.audio.removeEventListener('ended', this.endSoundHandlerBind);
 	};
 
 	this.pause = function() {
 		this.currentTime = audio.currentTime;
 		this.stop();
 	};
+
+	var endSoundHandler = function(e) {
+		this.currentTime = 0;
+		this.play();
+	};
+
+	this.endSoundHandlerBind = endSoundHandler.bind(this);
 };
